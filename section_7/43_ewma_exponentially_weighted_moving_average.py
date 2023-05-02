f6bcc3d6380c363288cbdf198ad22c98328bea87
Bruno Delgado <bruno.delgado@gympass.com>
2023-05-02 09:06:23 +0100

    S07E43 - EWMA - Exponentially weighted moving average

---
+++ section_7/43_ewma_exponentially_weighted_moving_average.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+airline = pd.read_csv('../Data/airline_passengers.csv', index_col='Month')
+airline.dropna(inplace=True)
+airline.index = pd.to_datetime(airline.index)
+
+print(airline.head())
+
+airline['6-month-SMA'] = airline['Thousands of Passengers'].rolling(window=6).mean()
+airline['12-month-SMA'] = airline['Thousands of Passengers'].rolling(window=12).mean()
+
+airline.plot(figsize=(12, 8))
+
+plt.show()
+
+airline['EWMA-12'] = airline['Thousands of Passengers'].ewm(span=12).mean()
+
+airline[['Thousands of Passengers', 'EWMA-12']].plot(figsize=(12, 8))
+
+plt.show()
